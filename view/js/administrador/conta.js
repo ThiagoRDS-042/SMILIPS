a2ab8194a1c0779586164bb80a5fc7a1649476f1
Thiago Rodrigues <thi013022001@gmail.com>
2021-04-08 11:44:55 -0300

    editar conta adm

---
+++ view/js/administrador/conta.js
@@ -0,0 +1,19 @@
+// capturando o campo de senha e o icone de visualizar
+let senhas = document.querySelectorAll("input[type=password]");
+let btns = document.querySelectorAll(".field-input i");
+
+// add um envento de click para mudar o tipo de input de password para text e o tipo de icone de visualizacao
+btns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    if (senhas[index].type == "password") {
+      senhas[index].type = "text";
+      btn.classList.remove("fa-eye");
+      btn.classList.add("fa-eye-slash");
+    } else {
+      senhas[index].type = "password";
+      btn.classList.add("fa-eye");
+      btn.classList.remove("fa-eye-slash");
+    }
+    senhas[index].focus();
+  });
+});
--- view/pages/administrador/conta.php
+++ view/pages/administrador/conta.php
@@ -22,7 +22,31 @@ admLogadoEntra();
   ?>
 
   <main>
+    <div class="edit-info">
+      <h1>Editar Conta</h1>
+      <form action="#" method="POST">
+        <div class="field-input">
+          <input type="text" name="email" required>
+          <span data-placeholder="Email"></span>
+        </div>
 
+        <div class="field-input">
+          <input type="password" name="senhaAtual" required class="senha">
+          <span data-placeholder="Senha Atual"></span>
+          <i class="fa fa-eye"></i>
+        </div>
+
+        <div class="field-input">
+          <input type="password" name="senhaNova" required class="senha">
+          <span data-placeholder="Nova Senha"></span>
+          <i class="fa fa-eye"></i>
+        </div>
+
+        <div class="field-button">
+          <button type="submit">Salvar</button>
+        </div>
+      </form>
+    </div>
 
   </main>
 
@@ -30,6 +54,8 @@ admLogadoEntra();
   <?php
   require_once('/xampp/htdocs/SMILIPS/view/pages/administrador/footer.php');
   ?>
+
+  <script src="/SMILIPS/view/js/administrador/conta.js"></script>
 </body>
 
 </html>
